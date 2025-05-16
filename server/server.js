08fa686fac5e57ccc3aab8221022eb2a77391786
Nguyễn Thị Diệu Linh <Linh.NTD225030@sis.hust.edu.vn>
2025-05-16 20:41:08 +0700

    update

--- server/server.js
+++ server/server.js
@@ -1,15 +1,10 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes";
-import multer from "multer";
-import path from "path";
 
 const app = express();
 app.use(cors());
 
-app.use("/uploads", express.static("uploads"));
-
-// Middleware để phân tích JSON và form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
